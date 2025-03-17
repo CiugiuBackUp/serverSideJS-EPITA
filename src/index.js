448cfcb8f286e6519d0bec22afedf84884e36aeb
CiugiuBackUp <corneliuciugurean@gmail.com>
2025-03-17 14:30:03 +0100

    01 final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import test from "./test.js";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// MIDDLEWARE
+app.use((req, res, next) => {
+  test();
+  const now = Date.now();
+  req.requestTime = now;
+  next();
+});
+// data sent to the user
+app.get("/", (req, res) => {
+  res.send(req.requestTime.toString());
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
