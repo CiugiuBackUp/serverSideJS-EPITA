591e89f83b749ec4e2ee910a2c9c338362fecb52
Ciugiu <corneliu.ciugurean@epita.fr>
2025-03-24 16:27:41 +0100

    feat: multer

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { userLogIn, userSignUp } from "../controllers/userControllers.js";
 import { verifyToken } from "../middleware/auth.js";
+import upload from "../middleware/multerConfig.js";
 const router = Router();
 
 router.post("/login", userLogIn);
 
 router.post("/signup", userSignUp);
 
-router.post("/test", verifyToken, (req, res) => {
-  res.send("test");
+router.put("/updateUser", verifyToken, upload.single("image"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  console.log(req.userId);
+  res.json({ message: "User response reached" });
 });
 export default router;
