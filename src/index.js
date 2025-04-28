54ac6b4b9ee4587f1f156bdbd1187ad03d615338
ciugiu <corneliu.ciugurean@epita.fr>
2025-04-28 20:12:52 +0200

    fix: cors

--- src/index.js
+++ src/index.js
@@ -19,12 +19,17 @@ import connectDB from "./utils/db.js";
 import hashPassword from "./middleware/passencrypt.js";
 
 // cors middleware
+// cors middleware
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
